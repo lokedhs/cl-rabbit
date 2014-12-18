c58dfc0adc4d8d37f55328a807980c2a1e439428
Elias Martenson <lokedhs@gmail.com>
2014-12-18 10:52:10 +0800

    Add an example function that runs the receiver in a thread

--- src/examples.lisp
+++ src/examples.lisp
@@ -8,6 +8,7 @@
       (channel-open conn 1)
       (basic-publish conn 1
                      :exchange "test-ex"
+                     :routing-key "xx"
                      :body (babel:string-to-octets "this is the message content" :encoding :utf-8)))))
 
 (defun recv-loop (conn)
@@ -20,10 +21,16 @@
       (socket-open socket "localhost" 5672)
       (login-sasl-plain conn "/" "guest" "guest")
       (channel-open conn 1)
-      (exchange-declare conn 1 "test-ex" "fanout")
+      (exchange-declare conn 1 "test-ex" "topic")
       (let ((queue-name (queue-declare conn 1 :auto-delete t)))
-        (queue-bind conn 1 :queue queue-name :exchange "test-ex")
+        (queue-bind conn 1 :queue queue-name :exchange "test-ex" :routing-key "xx")
         (basic-consume conn 1 queue-name)
         (let ((result (recv-loop conn)))
           (format t "Got message: ~s, content: ~s" result (babel:octets-to-string (message/body (envelope/message result))
                                                                                   :encoding :utf-8)))))))
+
+(defun test-recv-in-thread ()
+  (let ((out *standard-output*))
+    (bordeaux-threads:make-thread #'(lambda ()
+                                      (let ((*standard-output* out))
+                                        (test-recv))))))
