af991aa8d7e365dfb1a19a05c525f0b3ee66ea02
Elias Martenson <lokedhs@gmail.com>
2015-11-05 13:06:09 +0800

    Removed debug optimisation level

--- src/misc.lisp
+++ src/misc.lisp
@@ -1,7 +1,5 @@
 (in-package :cl-rabbit)
 
-(declaim (optimize (speed 0) (safety 3) (debug 3)))
-
 (defmacro unwind-when-fail (form &body unwind-forms)
   (let ((flag-sym (gensym "SUCCESS-"))
         (result-sym (gensym "RESULT-")))
