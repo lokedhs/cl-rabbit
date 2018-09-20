230af7a11d506aa1448d97116473b39621370a2f
Knut Olav Bøhmer <bohmer@gmail.com>
2018-09-20 15:13:24 +0200

    New macro with-channel

--- src/examples.lisp
+++ src/examples.lisp
@@ -1,34 +1,35 @@
 (in-package :cl-rabbit.examples)
 
+
 (defun test-send ()
   (with-connection (conn)
     (let ((socket (tcp-socket-new conn)))
       (socket-open socket "localhost" 5672)
       (login-sasl-plain conn "/" "guest" "guest")
-      (channel-open conn 1)
-      (basic-publish conn 1
-                     :exchange "test-ex"
-                     :routing-key "xx"
-                     :body "this is the message content"
-                     :properties '((:app-id . "Application id"))))))
+      (with-channel (conn 1)
+        (basic-publish conn 1
+                       :exchange "test-ex"
+                       :routing-key "xx"
+                       :body "this is the message content"
+                       :properties '((:app-id . "Application id")))))))
 
 (defun test-recv ()
   (with-connection (conn)
     (let ((socket (tcp-socket-new conn)))
       (socket-open socket "localhost" 5672)
       (login-sasl-plain conn "/" "guest" "guest")
-      (channel-open conn 1)
-      (exchange-declare conn 1 "test-ex" "topic")
-      (let ((queue-name "foo"))
-        (queue-declare conn 1 :queue queue-name)
-        (queue-bind conn 1 :queue queue-name :exchange "test-ex" :routing-key "xx")
-        (basic-consume conn 1 queue-name)
-        (let* ((result (consume-message conn))
-               (message (envelope/message result)))
-          (format t "Got message: ~s~%content: ~s~%props: ~s"
-                  result (babel:octets-to-string (message/body message) :encoding :utf-8)
-                  (message/properties message))
-          (cl-rabbit:basic-ack conn 1 (envelope/delivery-tag result)))))))
+      (with-channel (conn 1)
+        (exchange-declare conn 1 "test-ex" "topic")
+        (let ((queue-name "foo"))
+          (queue-declare conn 1 :queue queue-name)
+          (queue-bind conn 1 :queue queue-name :exchange "test-ex" :routing-key "xx")
+          (basic-consume conn 1 queue-name)
+          (let* ((result (consume-message conn))
+                 (message (envelope/message result)))
+            (format t "Got message: ~s~%content: ~s~%props: ~s"
+                    result (babel:octets-to-string (message/body message) :encoding :utf-8)
+                    (message/properties message))
+            (cl-rabbit:basic-ack conn 1 (envelope/delivery-tag result))))))))
 
 (defun test-recv-in-thread ()
   (let ((out *standard-output*))
